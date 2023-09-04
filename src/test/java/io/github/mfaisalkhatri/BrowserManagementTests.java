485f9299690c4cd5c47525d1f9a64ba976506028
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-09-04 11:53:10 +0530

    initial commit

---
+++ src/test/java/io/github/mfaisalkhatri/BrowserManagementTests.java
@@ -0,0 +1,35 @@
+package io.github.mfaisalkhatri;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class BrowserManagementTests {
+
+    @Test
+    public void testFirefoxVersion() {
+        final FirefoxOptions options = new FirefoxOptions();
+        options.setBrowserVersion("112");
+        // dev, beta, nightly
+        final WebDriver driver = new FirefoxDriver(options);
+        driver.navigate().to("https://www.selenium.dev/");
+        assertEquals (driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+
+    @Test
+    public void testChromeVersion() {
+        final ChromeOptions options = new ChromeOptions();
+        // stable, dev, beta, canary
+        options.setBrowserVersion("114");
+        final WebDriver driver = new ChromeDriver(options);
+        driver.navigate().to("https://www.selenium.dev/");
+        assertEquals (driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+}
---
+++ src/test/java/io/github/mfaisalkhatri/SeleniumDemoTests.java
@@ -0,0 +1,30 @@
+package io.github.mfaisalkhatri;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SeleniumDemoTests {
+
+
+    @Test
+    public void testOnChrome() {
+        final WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.selenium.dev/");
+        assertEquals (driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+
+    @Test
+    public void testOnFirefox() {
+        final WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("https://www.selenium.dev/");
+        assertEquals (driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+
+
+}
