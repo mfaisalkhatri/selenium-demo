b3b365375b9329fe32fb3f2f71c45d25a032ba7f
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2024-05-30 20:22:24 +0530

    Added example code for login scenarios  (#11)
    * added new package and page object classes for testing login page of lambdatest e-commerce website
    
    * added test with dataprovider to verify login scenarios
    
    * formatting changes done

---
+++ src/test/java/io/github/mfaisalkhatri/logintestdemo/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package io.github.mfaisalkhatri.logintestdemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+
+    private WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private WebElement emailAddressField() {
+        return driver.findElement(By.id("input-email"));
+    }
+
+    private WebElement passwordField() {
+        return driver.findElement(By.id("input-password"));
+    }
+
+    private WebElement loginBtn() {
+        return driver.findElement(By.cssSelector("input.btn"));
+    }
+
+    public void performLogin(String email, String password){
+        emailAddressField().clear();
+        emailAddressField().sendKeys(email);
+        passwordField().clear();
+        passwordField().sendKeys(password);
+        loginBtn().click();
+    }
+
+    public String getErrorMessageText() {
+        return driver.findElement(By.cssSelector("#account-login div.alert")).getText();
+    }
+
+}
---
+++ src/test/java/io/github/mfaisalkhatri/logintestdemo/pages/MyAccountPage.java
@@ -0,0 +1,19 @@
+package io.github.mfaisalkhatri.logintestdemo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MyAccountPage {
+    private final WebDriver driver;
+
+    public MyAccountPage(final WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public String getPageTitle() {
+        return driver.findElement(By.cssSelector("#content h2")).getText();
+    }
+
+
+}
