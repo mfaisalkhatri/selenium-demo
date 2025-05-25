e3b80a9b91dd5dc8cf80fd11baa874be4b58e538
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-05-25 21:45:07 +0300

    register user page objects created, test data wip

--- src/test/java/io/github/mfaisalkhatri/seleniumgridjenkins/pages/RegistrationPage.java
+++ src/test/java/io/github/mfaisalkhatri/seleniumgridjenkins/pages/RegistrationPage.java
@@ -1,8 +1,10 @@
 package io.github.mfaisalkhatri.seleniumgridjenkins.pages;
 
+import io.github.mfaisalkhatri.seleniumgridjenkins.testdata.RegistrationData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationPage {
     private final WebDriver driver;
@@ -11,7 +13,39 @@ public class RegistrationPage {
         this.driver = driver;
     }
 
+    public void registerNewUser (final RegistrationData registrationData) {
+        
+    }
+
+    private WebElement answerToSecurityQuestionField () {
+        return this.driver.findElement (By.id ("securityAnswerControl"));
+    }
+
+    private WebElement emailField () {
+        return this.driver.findElement (By.id ("emailControl"));
+    }
+
     private WebElement pageHeader () {
         return this.driver.findElement (By.cssSelector ("mat-card h1"));
+    }
+
+    private WebElement passwordField () {
+        return this.driver.findElement (By.id ("passwordControl"));
+    }
+
+    private WebElement registerButton () {
+        return this.driver.findElement (By.id ("registerButton"));
+    }
+
+    private WebElement repeatPasswordField () {
+        return this.driver.findElement (By.id ("repeatPasswordControl"));
+    }
+
+    private WebElement securityQuestionField () {
+        return this.driver.findElement (By.id ("mat-select-1"));
+    }
+
+    private void selectSecurityQuestion (final String question) {
+        new Select (securityQuestionField ()).selectByVisibleText (question);
     }
 }
--- src/test/java/io/github/mfaisalkhatri/seleniumgridjenkins/testdata/RegistrationData.java
+++ src/test/java/io/github/mfaisalkhatri/seleniumgridjenkins/testdata/RegistrationData.java
@@ -1,4 +1,10 @@
 package io.github.mfaisalkhatri.seleniumgridjenkins.testdata;
 
 public class RegistrationData {
+
+    private String email;
+    private String password;
+    private String securityAnswer;
+    private String securityQuestion;
+
 }
