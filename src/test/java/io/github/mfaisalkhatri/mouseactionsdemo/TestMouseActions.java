1b2c87f01eb89f7551faba8aabab022e113d5fa6
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-06-02 18:56:06 +0300

    added double click example code

--- src/test/java/io/github/mfaisalkhatri/mouseactionsdemo/TestMouseActions.java
+++ src/test/java/io/github/mfaisalkhatri/mouseactionsdemo/TestMouseActions.java
@@ -1,12 +1,29 @@
 package io.github.mfaisalkhatri.mouseactionsdemo;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class TestMouseActions extends BaseTest {
+
+    @Test
+    public void testDoubleClickAction () {
+        this.driver.get ("https://unixpapa.com/js/testmouse.html");
+        final WebElement clickHereText = this.driver.findElement (By.linkText ("click here to test"));
+        final Actions actions = new Actions (this.driver);
+        actions.doubleClick (clickHereText)
+            .build ()
+            .perform ();
+
+        final WebElement textBox = this.driver.findElement (By.tagName ("textarea"));
+        final String textBoxValue = textBox.getAttribute ("value");
+
+        assertTrue (textBoxValue.contains ("dblclick"));
+    }
 
     @Test
     public void testMouseClickAction () throws InterruptedException {
